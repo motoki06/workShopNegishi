9a1284bda268783234272514ef5611a2544ff360
motoki06 <6motoki@gmail.com>
2021-10-20 21:54:09 +0900

    10/25の課題1を作成。

---
+++ VUE/additional/1.4-1.7/1/practice.js
@@ -0,0 +1,35 @@
+// 停車駅一覧
+var chuoLineStations = [
+    {
+        "id": "JC12", // 駅のID
+        "name": "三鷹", // 駅名
+        "isSpecialRapidStop": true // 特快停車駅
+    },
+    {
+        "id": "JC13",
+        "name": "武蔵境",
+        "isSpecialRapidStop": false
+    },
+    {
+        "id": "JC14",
+        "name": "東小金井",
+        "isSpecialRapidStop": false
+    },
+    {
+        "id": "JC15",
+        "name": "武蔵小金井",
+        "isSpecialRapidStop": false
+    },
+    {
+        "id": "JC16",
+        "name": "国分寺",
+        "isSpecialRapidStop": true
+    }
+]
+
+var app = new Vue({
+    el: "#station-list",
+    data: {
+        stationArray: chuoLineStations
+    }
+});
